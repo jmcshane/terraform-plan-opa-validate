b3492b4e0ca98cac047c2118037aa5cd561a0573
james.mcshane <james.mcshane@objectpartners.com>
2019-12-21 11:23:22 -0600

    Completing terraform code and ready to validate rules

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ resource "azurerm_network_security_group" "nsg" {
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_network_security_rule" "opa-fail" {
+resource "azurerm_network_security_rule" "allow-everything" {
   name                        = "allow-the-world"
   priority                    = 100
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
-  source_port_range           = "443"
+  source_port_range           = "*"
   destination_port_range      = "*"
   source_address_prefix       = "Internet"
   destination_address_prefix  = "VirtualNetwork"
